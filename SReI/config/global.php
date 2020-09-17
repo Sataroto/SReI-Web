91fbd5fc547cd11360848ff188749c34b02ef612
orlandobel <orlando.bel.jimmy@gmail.com>
2020-09-17 17:18:29 -0500

    configuracion de api

--- SReI/config/global.php
+++ SReI/config/global.php
@@ -3,11 +3,19 @@ use Kreait\Firebase;
 use Kreait\Firebase\Factory;
 use Kreait\Firebase\Firestore;
 
+use GuzzleHttp\Client;
+
 $factory = (new Factory)->withServiceAccount(__DIR__.'/key_document.json');
 $firestore = $factory->createFirestore();
 $db = $firestore->database();
 
+$api = new Client([
+    'base_uri' => 'http://localhost:3001/API_SREI/',
+    'timeout' => 200,
+]);
+
 
 return [
     'firestore' => $db,
+    'api' => $api,
 ];
